9de978a2ed746e5d70f1e7e3994ba27660a8acd2
DongwonLee <dwlee0@gmail.com>
2017-08-30 17:32:16 +0900

    Add files via upload

--- Test1.py
+++ Test1.py
@@ -3,9 +3,14 @@ import urllib.request
 import xlsxwriter
 
 download_path = 'C:/Test/Test.xlsx'
+workbook = xlsxwriter.Workbook(download_path)
+worksheet = workbook.add_worksheet()
 
-stock_code_list = list()
-stock_name_list = list()
+row = 1
+col = 0
+worksheet.write(0, 0, 'CODE')
+worksheet.write(0, 1, 'NAME')
+
 for i in range(1, 11):
     page = urllib.request.urlopen('http://finance.daum.net/quote/marketvalue.daum?stype=P&page=' + str(i) + '&col=listprice&order=desc')
     soup = BeautifulSoup(page.read().decode('utf-8'), "html.parser")
@@ -13,19 +18,12 @@ for i in range(1, 11):
     for e in elements:
         stock_code = e.find('a')['href'][-6:]
         stock_name = e.find('a').contents[0]
-        stock_code_list.append(stock_code)
-        stock_name_list.append(stock_name)
-
-workbook = xlsxwriter.Workbook(download_path)
-worksheet = workbook.add_worksheet()
-
-row = 1
-col = 0
-
-for code, name in zip(stock_code_list, stock_name_list):
-    worksheet.write(row, col, code)
-    worksheet.write(row, col + 1, name)
-    row += 1
-
+        worksheet.write(row, col, stock_code)
+        worksheet.write(row, col + 1, stock_name)
+        row += 1
+    price = soup.findAll('td', {'class' : 'num'})
+    for e in price:
+        p1 = e.contents[0]
+        print(p1)
 
 workbook.close()
